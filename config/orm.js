dbe9079addb96f632e4d0a2fe733be9a7152a88b
Dave Cox <davedvst@gmail.com>
2019-02-19 18:11:27 -0800

    added helper functions to orm

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,27 @@
 var connection = require("../config/connection");
 
+function createQmarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+function translateSql(obj) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 var orm = {
   selectAll: function(table, cb) {
     var dbQuery = "SELECT * FROM " + table + ";";
@@ -10,26 +32,26 @@ var orm = {
       }
       cb(res);
     });
-  }
+  },
   insertOne: function(table, cols, vals, cb) {
     var dbQuery =
-    "INSERT INTO " +
-    table +
-    " (" +
-    cols.toString() +
-    ") " +
-    "VALUES (" +
-    createQmarks(vals.length) +
-    ") ";
+      "INSERT INTO " +
+      table +
+      " (" +
+      cols.toString() +
+      ") " +
+      "VALUES (" +
+      createQmarks(vals.length) +
+      ") ";
 
     console.log(dbQuery);
     connection.query(dbQuery, vals, function(err, res) {
-        if (err) {
-          throw err;
-        }
-        cb(res);
-      });
-  }
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
   updateOne: function(table, objColVals, condition, cb) {
     var dbQuery =
       "UPDATE " +
@@ -49,16 +71,15 @@ var orm = {
     });
   },
   deleteOne: function(table, condition, cb) {
-      var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
-      console.log(dbQuery);
+    var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+    console.log(dbQuery);
 
-      connection.query(dbQuery, function(err, res) {
-        if (err) {
-          throw err;
-        }
-        cb(res);
-      });
-
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
   }
-
 };
+module.exports = orm;
