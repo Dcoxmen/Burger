9cb1d72fa7d29e77be4143dd5c4f3b78bf710668
Dave Cox <davedvst@gmail.com>
2019-02-21 18:24:50 -0800

    set up heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
-var mysql = require("mysql");
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
