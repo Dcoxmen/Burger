73cc994b835824358ae7c917a2b85b3f79497873
Dave Cox <davedvst@gmail.com>
2019-02-22 17:14:49 -0800

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,5 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 module.exports = connection;
